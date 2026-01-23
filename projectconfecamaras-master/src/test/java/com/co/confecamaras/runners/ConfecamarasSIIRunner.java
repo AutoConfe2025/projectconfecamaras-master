ec3b296ac48ecb5cc6932cc723f2ddaec63e5fcb
Pipebaron29 <126822304+Pipebaron29@users.noreply.github.com>
2026-01-23 08:41:37 -0500

    Update ConfecamarasSIIRunner.java

--- projectconfecamaras-master/src/test/java/com/co/confecamaras/runners/ConfecamarasSIIRunner.java
+++ projectconfecamaras-master/src/test/java/com/co/confecamaras/runners/ConfecamarasSIIRunner.java
@@ -1,3 +1,9 @@
+package com.co.confecamaras.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -6,6 +12,7 @@
         plugin = {"pretty"}
 )
 public class ConfecamarasSIIRunner {
+
     /*------Tag General------*/
     // @general
     // @Certificados ok
