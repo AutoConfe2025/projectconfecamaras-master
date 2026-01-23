8f80435d4bead47a8665084676404731348e7829
Pipebaron29 <126822304+Pipebaron29@users.noreply.github.com>
2026-01-23 09:45:28 -0500

    Update LoginUsuarioPublico.java

--- projectconfecamaras-master/src/main/java/com/co/confecamaras/userinterfaces/Certificados/Demas/LoginUsuarioPublico.java
+++ projectconfecamaras-master/src/main/java/com/co/confecamaras/userinterfaces/Certificados/Demas/LoginUsuarioPublico.java
@@ -7,7 +7,7 @@ import net.serenitybdd.screenplay.targets.Target;
 public class LoginUsuarioPublico extends PageObject {
 
     public static Target TXT_CAMARA = Target.the("Cuadro de texto para Seleccionar la camara de comercio").
-            located(By.xpath("//*[@id='orderList']/div[1]/div[2]/div[2]/div/input[@placeholder='Buscar...']"));
+            located(By.xpath("//input[@class='p-orderlist-filter-input p-inputtext p-component']]"));
     public static Target BTN_CAMARA = Target.the("Seleccionar Icono camara para ingresar ").
             located(By.xpath("//span[@class='text-dark textCamara']"));
     public static Target TXT_USUARIO = Target.the("Ingresar el usuario en el cuadro de texto").
