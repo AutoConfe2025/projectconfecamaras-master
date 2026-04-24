aad96dadb22a40f970e00ca9bfd560ccf5d206ce
LuisFelipeBaronPerez <felipebaron92@gmail.com>
2026-04-24 09:22:52 -0500

    ultimos cambios

--- projectconfecamaras-master/src/test/java/com/co/confecamaras/runners/ConfecamarasSIIRunner.java
+++ projectconfecamaras-master/src/test/java/com/co/confecamaras/runners/ConfecamarasSIIRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "pretty",
                 "rerun:target/rerun.txt"
         }
-        ,tags = "@"
+//        ,tags = "@"
 
 )
 public class ConfecamarasSIIRunner {
